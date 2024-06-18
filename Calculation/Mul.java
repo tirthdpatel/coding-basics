80f43df6f694059c1d702428abeb8d7a043f7e2c
tirthdpatel <v.tirthpatel@gmail.com>
2024-06-18 23:29:03 +0530

    trial commit

--- Calculation/Mul.java
+++ Calculation/Mul.java
@@ -5,6 +5,7 @@ class Mul {
     public static void main(String[] args) {
        int num1 = 5;
        int num2 = 4 ;
+       int num3=9;
        Mul obj = new Mul ();
        int answer = obj.Mul(num1, num2) ;
        System.out.println(answer);
