a7d1531dab48b2ced5f412fc3f40cfd3dc167bec
tirthdpatel <v.tirthpatel@gmail.com>
2024-06-27 15:18:46 +0530

    codeforces

---
+++ Codeforces/A_Mahmoud_and_Ehab_and_the_even_odd_game.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class A_Mahmoud_and_Ehab_and_the_even_odd_game{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int a ;
+        if (x%2==0){
+x=2;
+System.out.println("Mahmoud");
+
+}
+else{
+            System.out.println("Ehab");
+            x=1;
+        }
+
+    }
+}
