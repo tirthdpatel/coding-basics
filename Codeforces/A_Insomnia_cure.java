d99c2eba6f00b9576eaef77ff116ae60ea1f7d47
tirthdpatel <v.tirthpatel@gmail.com>
2024-07-10 19:03:58 +0530

    codeforces

--- Codeforces/A_Insomnia_cure.java
+++ Codeforces/A_Insomnia_cure.java
@@ -11,11 +11,18 @@ public class A_Insomnia_cure {
         int n = sc.nextInt();
         int d = sc.nextInt();
         int w=0;
-        for (int i = 0; i <d; i++) {
+        
+        for (int i = 1; i <=d; i++) {
+            if(k>d||l>d||m>d||n>d){
+                System.out.println("0");
+                break;
+            }
             if(i%k==0||i%l==0||i%m==0||i%n==0){
                 w++;
             }
         }
+       if(w>0){
         System.out.println(w);
+       }
 sc.close();
     }}
