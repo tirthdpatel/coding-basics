4bd46a770f138ab9cc5e0db4e2242daa625622d3
tirthdpatel <v.tirthpatel@gmail.com>
2024-07-18 12:01:37 +0530

    codeforces

---
+++ Codeforces/B_Maximum_Multiple_Sum.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class B_Maximum_Multiple_Sum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a=0, b=0;
+            if (n >= 6) {
+                // for (int i = 0; i < n; i++) {
+                //     if (a + i * 2 <= n) {
+                //         a = a + i * 2;
+                //     } 
+                //     else {
+                        System.out.println(2);
+                //         break;
+                //     }
+                // }
+            }
+            // if(n==6){
+            //     System.out.println("2");
+            //}
+            if(n==5){
+                System.out.println("2");
+            }
+            if(n==4){
+                System.out.println("2");
+            }
+            if(n==3){
+                System.out.println("3");
+            }
+            if(n==2){
+                System.out.println("2");
+            }
+            if(n==1){
+                System.out.println("1");
+            }
+            
+            
+            
+        }
+        }  
+    }
+
