b491e80efa30856110cf668a6dce757d5db6fe29
tirthdpatel <v.tirthpatel@gmail.com>
2024-08-14 14:01:43 +0530

    organising

---
+++ Codeforces/900/A_Vasya_and_Socks.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class A_Vasya_and_Socks{
+
+    public static void main(String a[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int x = n;
+        for (int i = 1; i <= n; i++) {
+            if(i%m==0){
+                x=x+1;
+            }
+
+        }
+        Sout
+    }
+}
