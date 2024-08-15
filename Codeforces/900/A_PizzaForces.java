88ecdd6dcb2d0b3cd1ea8076d5c4ea3c6f9114a7
tirthdpatel <v.tirthpatel@gmail.com>
2024-08-15 20:22:30 +0530

    codeforces 900

---
+++ Codeforces/900/A_PizzaForces.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class A_PizzaForces{
+    public static void main(string a[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            if(a%10==0){
+System.out.println()
+            }
+        }
+    }
+}
